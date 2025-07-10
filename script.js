f4452d99e28b1b7f4896df4f86ec9b743e971a06
Farwa Tariq <farwa.tariq2434@gmail.com>
2025-07-10 16:49:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//  js assignments 4 legal and illegal
+
+// 1. Declare 3 variables in one statement.
+let a = 5, b = 10, c = 15;
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Legal variable names
+let userName;
+let _student;
+let $price;
+let user1;
+let Ferishayy_Hayat;
+
+// Illegal variable names
+// let 1user;       // Cannot start with a number
+// let user-name;   // Hyphens not allowed 
+// let var;         // "var" is a reserved keyword
+// let my variable; // Spaces not allowed
+// let @total;      // Special characters  not allowed
+
+// ------------//
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+document.writeln("<h1>Rules for naming JS variables</h1> <br>");
+document.writeln("<p>Variable names can only contain letters, numbers, underscores, and dollar signs.</p>");
+document.writeln("<p>For example: $my_1stVariable</p>");
+document.writeln("<p>Variables must begin with a letter, underscore, or dollar sign. For example: $name, _name, or name</p>");
+document.writeln("<p>Variable names are case sensitive.</p>");
+document.writeln("<p>Variable names should not be JS reserved keywords.</p>");
